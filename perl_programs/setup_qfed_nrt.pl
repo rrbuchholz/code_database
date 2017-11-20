8fbb82ee5793b4807de7d02245e9d67693bffef1
rrbuchholz <buchholz@ucar.edu>
2017-11-20 12:06:22 -0700

    Automated QFED dwold

--- perl_programs/setup_qfed_nrt.pl
+++ perl_programs/setup_qfed_nrt.pl
@@ -2,8 +2,8 @@
 
 #======================================================#
 #--- This Perl script sets up CAM-chem emissions for---#
-#--- forecast runs.
-#---    * downloads QFED CO
+#--- forecast runs.                                 ---#
+#---    * downloads QFED CO2 NRT                     ---#
 #---    * calls the NCL script to re-grid and create---#
 #---      CAM-chem emissions                        ---#
 #---    * uplopads new emissions to glade           ---#
@@ -11,32 +11,37 @@
 #---                               rrb Apr 03, 2016 ---#
 #======================================================#
 
-`cd /net/modeling1/data14b/buchholz/qfed/orig_0.25/`;
+$topdir = "/net/modeling1/data14b/buchholz/qfed/orig_0.25";
+`cd $topdir/`;
 
+#------------------------------
+# set up email correspondence
+  $to = 'buchholz@ucar.edu';
+  $from = 'buchholz@ucar.edu';
 
 #------------------------------
 # determine dates of run and current time
-chomp($current_date = `date +%Y%m%d`) ;
+chomp($today = `date +%Y%m%d`) ;
+chomp($current_date = `date --date='$today -1 day' +%Y%m%d`);
 chomp($time = `date +%H`);
 chomp($year = `date +%Y`) ;
 chomp($m = `date +%m`) ;
 
 #************************TEST***********************
-$current_date = 20171101;
-
 print "Assessing emission file for $current_date\n";  #DEBUG
 
 #------------------------------
 # set up locations
-$dir = "/net/modeling1/data14b/buchholz/qfed/orig_0.25/co_$year/";
-$fname = "*co.*$current_date*.nc4";
-$ftp_address = "ftp://iesa:\@ftp.nccs.nasa.gov/aerosol/emissions/QFED/v2.5r1/0.25/QFED/Y$year/M$m/";
+$dir = "$topdir/co2_nrt/";
+$camdir = "/net/modeling1/data14b/buchholz/qfed/cam_0.94x1.2/from_co2/nrt/";
+$fname = "*co2.*$current_date*.nc4";
+$ftp_address = "ftp://ftp.nccs.nasa.gov/qfed/0.25_deg/Y$year/M$m/";
 
 print "$ftp_address\n";  #DEBUG
 
 #------------------------------
 # open log to see last time processed
-$proclog = "/net/modeling1/data14b/buchholz/qfed/orig_0.25/.emission_processlog";
+$proclog = "$topdir/.emission_processlog";
 open(IN, "<$proclog");
 chomp(@lines = <IN>);
 $processed = grep { /$current_date/ } @lines;
@@ -46,7 +51,7 @@ print "Is current date processed: $processed\n";  #DEBUG
 
 #------------------------------
 # Data download
-open(OUT,">/net/modeling1/data14b/buchholz/qfed/orig_0.25/temp.out");
+open(OUT,">$topdir/temp.out");
 chomp($last_file = `ls $dir$fname*`);
 
 if ($last_file ne '' && $processed == 0){
@@ -62,11 +67,14 @@ else{
   # download not there
   print OUT "$current_date, hour $time: Emissions neither downloaded or processed\n";
   print OUT "Downloading . . . \n";
-  print OUT  "wget $ftp_address$fname\n";
+  print OUT  "wget --user=gmao_ops --password= $ftp_address$fname\n";
      `wget -N -q -P $dir "$ftp_address$fname" `;
 }
 
+      $codehome = "/home/buchholz/Documents/code_database/ncl_programs/data_processing";
+     print "ncl YYYYMMDD=$current_date $codehome/combine_qfed_finn_ers.ncl > $topdir/out.dat\n";
 
+#exit
 #------------------------------
 # process the emission file
 chomp($check_again = `ls $dir$fname*`);
@@ -74,27 +82,41 @@ chomp($check_again = `ls $dir$fname*`);
 if ($check_again ne '' && $processed == 0){
   # download there and not done
   print OUT "Processing still needed, performing . . .\n";
-     # --- add in NCL processing script ---#;
+     # --- add in call to NCL processing script ---#
+      $codehome = "/home/buchholz/Documents/code_database/ncl_programs/data_processing";
+     `ncl YYYYMMDD=$current_date $codehome/combine_qfed_finn_ers.ncl > $topdir/out.dat`;
 
-  open(OUT2,">>$proclog");
-  print OUT2 "processed $current_date\n";
-  close(OUT2);
+  chomp($last_file = `ls $dir$fname*`);
+    open(OUT2,">>$proclog");
+    print OUT2 "processed $current_date\n";
+    close(OUT2);
+
+    #send email once processed
+    $subject = 'QFED '.$current_date.' processing complete';
+    $message = '*** Completed processing QFED for use in CAM-chem for '. $current_date .'.';
+    open(MAIL, "|/usr/sbin/sendmail -t");
+    # Email Header
+    print MAIL "To: $to\n";
+    print MAIL "From: $from\n";
+    print MAIL "Subject: $subject\n\n";
+    # Email Body
+    print MAIL $message;
+    close(MAIL);
+    print OUT "Email Sent Successfully\n";
 }
 
   close(OUT);
 #------------------------------
-#send to glade at 10 pm
-if ($time == 22 && $processed == 1){
+#send to glade at 5am
+if ($time == 5 && $processed == 1){
 
 }
 
 #------------------------------
 #send email if processing not done by 1pm
-if (($time == 13 || $time == 22)&& $processed == 0){
-  $to = 'buchholz@ucar.edu';
-  $from = 'buchholz@ucar.edu';
+if (($time == 4 || $time == 10)&& $processed == 0){
   $subject = 'QFED processing not done';
-  $message = 'After 1pm, QFED processing not done for '. $current_date .', may need to manually process.';
+  $message = 'After 4am, QFED processing not done for '. $current_date .', may need to manually process.';
 
   open(MAIL, "|/usr/sbin/sendmail -t");
 
