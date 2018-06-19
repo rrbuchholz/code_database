ad0d1d5bed71b4ce24970e7f70965e77ee224aa7
rrbuchholz <buchholz@ucar.edu>
2018-06-18 19:13:54 -0600

    BB inventory analysis

--- perl_programs/subset_CAMchem.pl
+++ perl_programs/subset_CAMchem.pl
@@ -1,33 +1,36 @@
 #!/usr/bin/perl -w
 #
-# Script for subsetting a region from CAMchem output
+# Script for subsetting species or a region from CAMchem output
 #
-
-$rundir = "/glade/scratch/buchholz/fmerra.e15alpha.FSDSSOA.2deg.longrun/h1/";
+$runtype = "gfasBBCO";
+$rundir = "/glade/scratch/buchholz/archive/fmerra.2.0.FCSD.1deg.chey180617.cmip.".$runtype."/atm/hist/";
 #$rundir = "/glade/scratch/buchholz/CAMchem_fmerra_e15_BAM_constE/";
+$outdir = "/glade2/work/buchholz/CAM_chem_output/fire_uncert/".$runtype."/";
 #------------------------------------
 # create tracer list of tagged tracers to extract
 #------------------------------------
 $tracerlist = "CO";
-for  $s (0..29) {
-  print"CO".sprintf("%02d",$s+1).", ";
-  $tracerlist = $tracerlist.",CO".sprintf("%02d",$s+1);
-}
+#for  $s (0..29) {
+#  print"CO".sprintf("%02d",$s+1).", ";
+#  $tracerlist = $tracerlist.",CO".sprintf("%02d",$s+1);
+#}
 print "\n";
 
 #------------------------------------
 # concatenate files
 #------------------------------------
-for  $i (2011..2014) {
+for  $i (2014..2014) {
   $y =  sprintf("%04d",$i);
-  #$outfile = $rundir."CAM_chem_fmerra_FSDSSOA_2deg_".$y."_Australasia.nc";
-  $outfile = $rundir."CAM_chem_fmerra_FSDSSOA_2deg_".$y."_Australasia.nc";
+  $outfile = $outdir."CAM_chem_fmerra2_FCSD_1deg_".$runtype."_".$y.".nc";
   print "$outfile\n";
-  chomp(@to_combine = `ls $rundir*$y-*.nc`);
+  chomp(@to_combine = `ls $rundir*h1.*$y-*.nc`);
   print "Combining $y\n";
 
-  print "ncrcat -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,20,43 -d lon,44,73 @to_combine $outfile\n";
-  `ncrcat -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,20,43 -d lon,44,73 @to_combine $outfile`;
+  print "ncrcat -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PS,$tracerlist @to_combine $outfile\n";
+  `ncrcat -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PS,$tracerlist @to_combine $outfile`;
+
+  #print "ncrcat -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,20,43 -d lon,44,73 @to_combine $outfile\n";
+  #`ncrcat -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,20,43 -d lon,44,73 @to_combine $outfile`;
 }
 
 
