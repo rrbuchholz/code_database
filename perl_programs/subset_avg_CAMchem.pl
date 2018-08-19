2bda848d8d5cc866f6da8eeaa931ac29d14a0d93
rrbuchholz <buchholz@ucar.edu>
2018-08-19 13:38:59 -0600

    Ethane analysis

--- perl_programs/subset_avg_CAMchem.pl
+++ perl_programs/subset_avg_CAMchem.pl
@@ -3,32 +3,36 @@
 # Script for subsetting a region from CAMchem output
 #
 
-$rundir = "/glade/scratch/buchholz/fmerra.e15alpha.FSDSSOA.2deg.longrun/h1/";
+#$rundir = "/glade/scratch/buchholz/fmerra.e15alpha.FSDSSOA.2deg.longrun/h1/";
 #$rundir = "/glade/scratch/buchholz/CAMchem_fmerra_e15_BAM_constE/";
+$rundir = "/data16a/buchholz/CAM_chem_output/fmerra.208.FCSD.1deg.chey180418/atm/h0/";
+$outdir = "/data16a/buchholz/CAM_chem_output/fmerra.208.FCSD.1deg.chey180418/processed/";
 #------------------------------------
 # create tracer list of tagged tracers to extract
 #------------------------------------
-$tracerlist = "CO";
-for  $s (12,15,16,26,29,30) {
-  #print"CO".sprintf("%02d",$s).", ";
-  $tracerlist = $tracerlist.",CO".sprintf("%02d",$s);
-}
+$tracerlist = "CO,C2H6";
+#for  $s (12,15,16,26,29,30) {
+#  #print"CO".sprintf("%02d",$s).", ";
+#  $tracerlist = $tracerlist.",CO".sprintf("%02d",$s);
+#}
 print "$tracerlist\n";
 #print "\n";
 #------------------------------------
 # concatenate files
 #------------------------------------
 for  $i (2014..2014) {
-  for  $j (1..11) {
+  #for  $j (1..12) {
     $y =  sprintf("%04d",$i);
     $m =  sprintf("%02d",$j);
-    $outfile = $rundir."CAM_chem_fmerra_FSDSSOA_2deg_".$y.$m."_MtTronadorRegion_tags.nc";
+    $outfile = $outdir."CAM_chem_fmerra_FCSD_1deg_".$y.$m."_ethane.nc";
     print "$outfile\n";
-    chomp(@to_combine = `ls $rundir*$y-$m-*.nc`);
+    #chomp(@to_combine = `ls $outdir*$y-$m-*.nc`);
+    chomp(@to_combine = `ls $outdir*$y-$m-*.nc`);
     print "Averaging $y$m\n";
 
-    print "ncra -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,23,28 -d lon,113,118 @to_combine $outfile\n";
-    `ncra -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,23,28 -d lon,113,118 @to_combine $outfile`;
+    print "ncra -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist  @to_combine $outfile\n";
+    #`ncra -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist -d lat,23,28 -d lon,113,118 @to_combine $outfile`;
+    `ncra -O -v date,datesec,time,lat,lon,P0,hyam,hybm,hyai,hybi,PDELDRY,$tracerlist @to_combine $outfile`;
   }
 }
 
--- perl_programs/timeavg_CAMchem.pl
+++ perl_programs/timeavg_CAMchem.pl
@@ -3,18 +3,20 @@
 # Script for temporally averaging CAMchem output
 #
 
-$rundir = "/glade2/scratch2/buchholz/archive/fmerra.208.FCSD.1deg.chey180418.noanth/atm/h0/";
-$outdir = "/glade/p/work/buchholz/data_processing/CAM-chem/FCSD2018_noanth/";
+#$rundir = "/glade2/scratch2/buchholz/archive/fmerra.208.FCSD.1deg.chey180418.noanth/atm/h0/";
+#$outdir = "/glade/p/work/buchholz/data_processing/CAM-chem/FCSD2018_noanth/";
+$rundir = "/data16a/buchholz/CAM_chem_output/fmerra.208.FCSD.1deg.chey180418/atm/h0/";
+$outdir = "/data16a/buchholz/CAM_chem_output/fmerra.208.FCSD.1deg.chey180418/processed/";
 #------------------------------------
 # create tracer list of tagged tracers to extract
 #------------------------------------
-$tracerlist = "date,datesec,time,lat,lon,P0,PS,hyam,hybm,hyai,hybi,CO,C2H6,ISOP,NO2,NO,NOX,NOY,O3,OH,PAN";
+$tracerlist = "date,datesec,time,lat,lon,P0,PS,PDELDRY,hyam,hybm,hyai,hybi,CO,C2H6,ISOP,NO2,NO,NOX,NOY,O3,OH,PAN";
 print "$tracerlist\n";
 #print "\n";
 #------------------------------------
 # concatenate files
 #------------------------------------
-for  $i (2005..2014) {
+for  $i (2008..2015) {
   $to_combine = "";
   for  $j (1..12) {
     $m =  sprintf("%02d",$j);
@@ -22,16 +24,16 @@ for  $i (2005..2014) {
     $to_combine = $to_combine.$fname." ";
   }
     print "Averaging $i$m\n";
-    $outfile = $outdir."CAM_chem_fmerra_FCSD208_1deg_noanth_".$i.$m."_output_dummy.nc";
+    $outfile = $outdir."CAM_chem_fmerra_fmerra_FCSD_1deg_".$i."_output_dummy.nc";
 
     print "ncra -O -v $tracerlist $to_combine $outfile\n";
     `ncra -O -v $tracerlist $to_combine $outfile`;
 }
      chomp(@final_average = `ls $outdir*dummy.nc`);
          print "Averaging all\n";
-         $outfile = $outdir."CAMchem_2001_2007";
+         $outfile = $outdir."CAMchem_2008_2015";
          print "ncra -O -v $tracerlist @final_average $outfile.nc\n";
-         `ncra -O -v $tracerlist @final_average $outdir"CAMchem_FCSD208_1deg_noanth_2001_2007.nc"`;
+         `ncra -O -v $tracerlist @final_average $outdir"CAMchem_FCSD208_1deg_2008_2015.nc"`;
 
 
 
