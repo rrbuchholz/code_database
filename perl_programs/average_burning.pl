29193fd8449ea9d0a08484fba718be62ef783cde
rrbuchholz <buchholz@ucar.edu>
2018-08-28 16:04:31 -0600

    Averaging FRP

--- perl_programs/average_burning.pl
+++ perl_programs/average_burning.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl -w
 #
-# Script for averaging burned area month data
+# Script for averaging summed years of burned area month data
 #
 
 $rundir = "/IASI/home/buchholz/burned_area/GFED_4/";
+$outdir = "/IASI/home/buchholz/burned_area/averages/";
 #------------------------------------
 # create tracer list of tagged tracers to extract
 #------------------------------------
@@ -14,10 +15,10 @@ $tracerlist = "BurnedArea";
     #------------------------------------
     # concatenate files
     #------------------------------------
-    for  $i (2014..2014) {
+    for  $i (2010..2015) {
          $to_combine = "";
-         $dummy_sum = $i."_sum_01to12"."_2014_dummy.nc";
-         $outfile_sum = $i."_sum_01to12"."_2014.nc";
+         $dummy_sum = $i."_sum_01to12_dummy.nc";
+         $outfile_sum = $i."_sum_01to12"."_2011to2016.nc";
          print"$to_combine\n";
          for  $j (1..12) {
           $dateval = $i.sprintf("%02d",$j);
@@ -36,32 +37,35 @@ $tracerlist = "BurnedArea";
           $to_combine = $to_combine.$fname." ";
 
          #------------------------------------
-         # collect files to avergae
+         # collect year sum files to average
          #------------------------------------
            if ($j == 1){
-             `cp $fname $dummy_sum`;
+             `cp $fname $outdir$dummy_sum`;
+
            } else {
              #ncbo can only handle three filenames
-             print "ncbo -O -v $tracerlist --op_typ=+ $fname $dummy_sum $outfile_sum \n";
-             `ncbo -O -v $tracerlist --op_typ=+ $fname $dummy_sum $outfile_sum`;
-             print "cp $outfile_sum $dummy_sum \n";
-             `cp $outfile_sum $dummy_sum`;
+             print "ncbo -O -v $tracerlist --op_typ=+ $fname $outdir$dummy_sum $outdir$outfile_sum \n";
+             `ncbo -O -v $tracerlist --op_typ=+ $fname $outdir$dummy_sum $outdir$outfile_sum`;
+             print "cp $outdir$outfile_sum $outdir$dummy_sum \n";
+             `cp $outdir$outfile_sum $outdir$dummy_sum`;
+
            }
          }
          print "\n";
 
          #$outfile = $i."_dummy.nc";
-         #$outfile = $i."_08to09"."_dummy.nc";
-         $outfile = $i."_sum_01to12"."_2014.nc";
+         #$outfile = $i."_01to12"."_BA_dummy.nc";
+         #$outfile = $i."_sum_01to12"."_2014.nc";
+         #$outfile = $i."_sum_01to12"."_2011to2016.nc";
          #print "$outfile\n";
          #chomp(`ls $rundir$i*/month/*.he5`);
 
          #------------------------------------
          #print "Averaging $i\n";
         # note ncra requires a time dimension in files, nces requires no time dimension
-         print "nces -O -v $tracerlist $to_combine$outfile\n";
+         #print "nces -O -v $tracerlist $to_combine$outfile\n";
         #`nces -O -v $tracerlist $to_combine$outfile`;
-exit
+
          #------------------------------------
          # adding for a year before averaging
          # (need to do a couple of hack steps)
@@ -77,10 +81,11 @@ exit
      }
 
 
-     chomp(@final_average = `ls *dummy.nc`);
+     chomp(@final_average = `ls $outdir*dummy.nc`);
+         $finalout = "burnarea_2001to2016.nc";
          print "Averaging all\n";
-         print "nces -O -v $tracerlist @final_average burnarea_2014.nc\n";
-         `nces -O -v $tracerlist @final_average burnarea_2014.nc`;
+         print "nces -O -v $tracerlist @final_average $outdir$finalout\n";
+         `nces -O -v $tracerlist @final_average $outdir$finalout`;
 
 
 
