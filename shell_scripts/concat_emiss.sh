39c172b5aa681e75fb7e9f34d5a3ededfa5c311a
rrbuchholz <buchholz@ucar.edu>
2018-08-29 11:24:14 -0600

    BB inventory analysis

--- shell_scripts/concat_emiss.sh
+++ shell_scripts/concat_emiss.sh
@@ -6,12 +6,12 @@
 # (netCDF operators)
 #-------------------
 
-workdir=/data14b/buchholz/qfed/cam_0.9x1.25/from_co2/v2.5/   # working directory
+#workdir=/data14b/buchholz/qfed/cam_0.9x1.25/from_co2/v2.5/   # working directory
 #workdir=/data14b/buchholz/qfed/cam_0.9x1.25/regridded/   # working directory
 #workdir=/data14b/buchholz/gfas/cam_0.9x1.25/from_co2/   # working directory
 #workdir=/data14b/buchholz/gfas/cam_0.9x1.25/regridded/   # working directory
-#workdir=/data14b/buchholz/gfas/cam_0.9x1.25/regridded/   # working directory
-file_list="$(ls $workdir/2014_min/)"
+workdir=/data14b/buchholz/gfas/cam_0.9x1.25/regridded/   # working directory
+file_list="$(ls $workdir/2014/)"
 arr=$(echo "$file_list" | sed 's/2014*.nc//g')        # remove year specific to file
 #arr=$(echo "$file_list" | sed 's/2014new*.nc//g')        # remove year specific to file (gfas)
 
@@ -33,10 +33,10 @@ for x in $arr ; do
   #       ${workdir}2010/${x}2010.nc \
   #       ${workdir}2011/${x}2011.nc \
   #       ${workdir}2012/${x}2012.nc \
-  ncrcat ${workdir}2013_min/${x}2013.nc \
-         ${workdir}2014_min/${x}2014.nc \
-         ${workdir}2015_min/${x}2015.nc \
-         ${workdir}2016_min/${x}2016.nc \
-         ${workdir}2017_min/${x}2017.nc \
-         ${workdir}allyears/${x}2013_2017.nc       # Concatenate along
+  #ncrcat ${workdir}2013_min/${x}2013.nc \
+  ncrcat ${workdir}2014/${x}2014.nc \
+         ${workdir}2015/${x}2015.nc \
+         ${workdir}2016/${x}2016.nc \
+         ${workdir}2017/${x}2017.nc \
+         ${workdir}allyears/${x}2014_2017.nc       # Concatenate along
 done
