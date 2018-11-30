bc82de711ccb69dfed04caf2b7269d4e91cb63a0
rrbuchholz <buchholz@ucar.edu>
2018-11-30 13:08:11 -0700

    MOPITT plotting code updates

--- perl_programs/average_mopitt.pl
+++ perl_programs/average_mopitt.pl
@@ -3,7 +3,7 @@
 # Script for averaging MOPITT month data
 #
 
-$rundir = "/net/mopfl/MOPITT/V7J/Archive/L3/";
+$rundir = "/net/mopfl/MOPITT/V8T/Archive/L3/";
 #$rundir = "/MOPITT/V7J/Archive/L3/";
 #------------------------------------
 # create tracer list of tagged tracers to extract
@@ -14,7 +14,7 @@ $tracerlist = "RetrievedCOTotalColumnDay,RetrievedCOTotalColumnNight,RetrievedCO
     #------------------------------------
     # concatenate files
     #------------------------------------
-    for  $i (2014..2014) {
+    for  $i (2002..2017) {
          $to_combine = "";
          print"$to_combine\n";
          for  $j (1..12) {
@@ -26,7 +26,7 @@ $tracerlist = "RetrievedCOTotalColumnDay,RetrievedCOTotalColumnNight,RetrievedCO
 
          #$outfile = "/net/mopfl/home/buchholz/MOPITT_subset/V7/averages/".$i."_dummy.nc";
          #$outfile = "/net/mopfl/home/buchholz/MOPITT_subset/V7/averages/".$dateval."_dummy.nc";
-         $outfile = "/net/mopfl/home/buchholz/MOPITT_subset/V7/averages/".$dateval."_01to12.nc";
+         $outfile = "/net/mopfl/home/buchholz/MOPITT_subset/V8/averages/".$dateval."_01to12.nc";
          #print "$outfile\n";
          #chomp(`ls $rundir$i*/month/*.he5`);
          print "Averaging $i\n";
@@ -34,10 +34,9 @@ $tracerlist = "RetrievedCOTotalColumnDay,RetrievedCOTotalColumnNight,RetrievedCO
         # note ncra requires a time dimension in files, nces requires no time dimension
          print "nces -O -v $tracerlist $to_combine$outfile\n";
         `nces -O -v $tracerlist $to_combine$outfile`;
-     exit
      }
 
-     chomp(@final_average = `ls /net/mopfl/home/buchholz/MOPITT_subset/V7/averages/*dummy.nc`);
+     chomp(@final_average = `ls /net/mopfl/home/buchholz/MOPITT_subset/V8/averages/*_01to12.nc`);
          print "Averaging all\n";
-         print "nces -O -v $tracerlist @final_average /net/mopfl/home/buchholz/MOPITT_subset/V7/averages/MOPITT_2001_2016.nc\n";
-         `nces -O -v $tracerlist @final_average /net/mopfl/home/buchholz/MOPITT_subset/V7/averages/MOPITT_2001_2016.nc`;
+         print "nces -O -v $tracerlist @final_average /net/mopfl/home/buchholz/MOPITT_subset/V8/averages/MOPITT_2002_2017.nc\n";
+         `nces -O -v $tracerlist @final_average /net/mopfl/home/buchholz/MOPITT_subset/V8/averages/MOPITT_2002_2017.nc`;
--- perl_programs/sd_mopitt.pl
+++ perl_programs/sd_mopitt.pl
@@ -4,13 +4,23 @@
 # Use after averaging script
 #
 
-$rundir = "/IASI/home/buchholz/scripts/";
-$avgfile = "MOPITT_SOND_2001_2016.nc";
+$rundir = "/net/mopfl/home/buchholz/MOPITT_subset/V8/averages/";
+$mopdir = "/net/mopfl/MOPITT/V8T/Archive/L3/";
+$avgfile = $rundir."MOPITT_2002_2017.nc";
 
 #------------------------------------
 # create tracer list of tagged tracers to extract
 #------------------------------------
-chomp(@month_files = `ls *dummy.nc`);
+#chomp(@month_files = `ls *dummy.nc`);
+    for  $i (2002..2017) {
+         for  $j (1..12) {
+          $dateval = $i.sprintf("%02d",$j);
+          chomp($fname = `ls $mopdir$dateval/month/*.he5`);
+          $to_combine = $to_combine.$fname." ";
+         }
+     }
+    @month_files = split(/ /, $to_combine);
+
 $dims = scalar(@month_files);
 print "Number of files = $dims\n";
 
@@ -22,16 +32,16 @@ $tracerlist = "RetrievedCOTotalColumnDay,RetrievedCOTotalColumnNight,RetrievedCO
     #------------------------------------
     for  $i (0..$dims-1) {
       chomp($infile = @month_files[$i]); 
-      my @str_split = split(/_/, $infile);
-      $outfile = @str_split[0]."_diff.nc";
+      my @str_split = split(/-/, $infile);
+      $outfile = $rundir.@str_split[1]."_diff.nc";
       print "$outfile\n";
    
       print "ncbo -O -v $tracerlist --op_typ=- $infile $avgfile $outfile \n";
       `ncbo -O -v $tracerlist --op_typ=- $infile $avgfile $outfile`;
      }
 
-      chomp(@final_diffs = `ls *_diff.nc`);
-      $sdfile = "MOPITT_2001_2016_SONDsd.nc";
+      chomp(@final_diffs = `ls $rundir*_diff.nc`);
+      $sdfile = $rundir."MOPITT_2002_2017_sd.nc";
       print "--------------------------------------------------\n";
       print "nces -v $tracerlist -y rmssdn @final_diffs $sdfile \n";
       `nces -O -v $tracerlist -y rmssdn @final_diffs $sdfile`;
