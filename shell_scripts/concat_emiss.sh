19ea7de899644fc444773962f10adbd3678f3e01
rrbuchholz <buchholz@ucar.edu>
2019-01-30 10:28:25 -0700

    emission analysis

--- shell_scripts/concat_emiss.sh
+++ shell_scripts/concat_emiss.sh
@@ -7,8 +7,8 @@
 #-------------------
 
 # set up directories
-workdir=/data14b/buchholz/qfed/cam_0.9x1.25/from_co2/nrt/   # working directory
-#workdir=/data14b/buchholz/qfed/cam_0.9x1.25/from_co2/v2.5/   # working directory
+#workdir=/data14b/buchholz/qfed/cam_0.9x1.25/from_co2/nrt/   # working directory
+workdir=/data14b/buchholz/qfed/cam_0.9x1.25/from_co2/v2.5/   # working directory
 #workdir=/data14b/buchholz/qfed/cam_0.9x1.25/regridded/   # working directory
 #workdir=/data14b/buchholz/gfas/cam_0.9x1.25/from_co2/   # working directory
 #workdir=/data14b/buchholz/gfas/cam_0.9x1.25/regridded/   # working directory
@@ -25,28 +25,28 @@ for x in $arr ; do
     echo ${x}2018.nc \
 
 #concatenate
-#  ncrcat ${workdir}2000/${x}2000.nc \
-#         ${workdir}2001/${x}2001.nc \
-#         ${workdir}2002/${x}2002.nc \
-#         ${workdir}2003/${x}2003.nc \
-#         ${workdir}2004/${x}2004.nc \
-#         ${workdir}2005/${x}2005.nc \
-#         ${workdir}2006/${x}2006.nc \
-#         ${workdir}2007/${x}2007.nc \
-#         ${workdir}2008/${x}2008.nc \
-#         ${workdir}2009/${x}2009.nc \
-#         ${workdir}2010/${x}2010.nc \
-#         ${workdir}2011/${x}2011.nc \
-#         ${workdir}2012/${x}2012.nc \
-#         ${workdir}2013/${x}2013.nc \
-#         ${workdir}2014/${x}2014.nc \
-#         ${workdir}2015/${x}2015.nc \
-#         ${workdir}2016/${x}2016.nc \
-#         ${workdir}2017/${x}2017.nc \
-#         ${workdir}2017/${x}2018.nc \
-#         ${workdir}allyears/${x}2018_2020.nc       # Concatenate along
-ncrcat    ${x}2018.nc \
-          ${x}2019.nc \
-          ${x}2020.nc \
-          ${x}2018_2020.nc       # Concatenate along
+  ncrcat ${workdir}2000/${x}2000.nc \
+         ${workdir}2001/${x}2001.nc \
+         ${workdir}2002/${x}2002.nc \
+         ${workdir}2003/${x}2003.nc \
+         ${workdir}2004/${x}2004.nc \
+         ${workdir}2005/${x}2005.nc \
+         ${workdir}2006/${x}2006.nc \
+         ${workdir}2007/${x}2007.nc \
+         ${workdir}2008/${x}2008.nc \
+         ${workdir}2009/${x}2009.nc \
+         ${workdir}2010/${x}2010.nc \
+         ${workdir}2011/${x}2011.nc \
+         ${workdir}2012/${x}2012.nc \
+         ${workdir}2013/${x}2013.nc \
+         ${workdir}2014/${x}2014.nc \
+         ${workdir}2015/${x}2015.nc \
+         ${workdir}2016/${x}2016.nc \
+         ${workdir}2017/${x}2017.nc \
+         ${workdir}2018/${x}2018.nc \
+         ${workdir}allyears/${x}2000_2018_newEF.nc       # Concatenate along
+#ncrcat    ${x}2018.nc \
+#          ${x}2019.nc \
+#          ${x}2020.nc \
+#          ${x}2018_2020.nc       # Concatenate along
 done
